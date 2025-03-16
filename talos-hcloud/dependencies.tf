56515b37302e060d29b3ba40e36f7339c806aa63
Sebastian Mangelsdorf <16096537+Cardes@users.noreply.github.com>
2025-03-16 11:52:43 +0100

    change hcloud-talos

--- talos-hcloud/dependencies.tf
+++ talos-hcloud/dependencies.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     talos-hcloud = {
-        source = "hcloud-talos/talos/hcloud"
+        source = "hcloud-talos/talos"
         version = "2.13.1"
         }
   }
